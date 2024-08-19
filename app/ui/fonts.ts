400ef61b974e373bfaad70fa0e281c498002cd00
Edgar Canro <edgar.canro@gmail.com>
2024-08-19 21:48:38 +0100

    second commit
    tutorial next

---
+++ app/ui/fonts.ts
@@ -0,0 +1,10 @@
+import { Inter } from 'next/font/google';
+import { Lusitana } from 'next/font/google';
+
+
+export const inter = Inter({ subsets: ['latin']});
+export const lusitana = Lusitana({ 
+    subsets: ['latin'],
+    weight: ['400', '700'], /** opcional si pide que indique un weight */
+    preload: false /**evita la carga inmediata */
+});
